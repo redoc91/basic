c434fbeceabb5257407532d603afaddd0105ff9f
redoc91 <tarun_singhal1991@hotmail.com>
2025-06-10 01:27:13 +0530

    squash commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Hello, World!");
+setInterval(() => {
+  console.log("Hello, World!");
+}, 1000);
+// This code prints "Hello, World!" to the console every second.
+// It uses setInterval to repeatedly execute the console.log statement. 
