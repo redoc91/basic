d1bbbdea6ced4bf55c75b2f41674ffcb5910be70
redoc91 <tarun_singhal1991@hotmail.com>
2025-06-23 19:20:40 +0530

    updated index js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-console.log("Hello, World!");
+console.log("Hello, new World!");
 setInterval(() => {
-  console.log("Hello, World!");
+  console.log("Hello, new World!");
 }, 1000);
-// This code prints "Hello, World!" to the console every second.
+// This code prints "Hello, new World!" to the console every second.
 // It uses setInterval to repeatedly execute the console.log statement. 
